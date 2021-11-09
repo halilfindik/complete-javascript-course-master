734945788881e3d6dfff0b11061aeb06ad703e43
halilfindik <halfin@gmail.com>
2021-11-09 19:26:41 +0300

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is fun!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+let country = "Turkiye"
+let continent = "Avrasya"
+let population = "80 Milyon"
+
+console.log()
